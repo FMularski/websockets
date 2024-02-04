3336d24e55daf6c8f6eba4a6b4d68220d47c4f48
Filip Mularski <mularskif@gmail.com>
2024-02-04 01:10:27 +0100

    implemented middleware to authenticate user with token over ws

--- blog/asgi.py
+++ blog/asgi.py
@@ -11,16 +11,16 @@ import os
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 from comments import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
-
 django_asgi_app = get_asgi_application()
+
+from comments.middlewares import TokenAuthMiddleware
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
+    "websocket": TokenAuthMiddleware(
         URLRouter(routing.websocket_urlpatterns)
     )
 })
--- comments/consumers.py
+++ comments/consumers.py
@@ -3,6 +3,12 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class CommentConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        print(self.scope.__dict__)
-        
-        await self.accept()
+        self.user = self.scope["user"]
+
+        if self.user.is_authenticated:
+            await self.accept()
+        else:
+            await self.close()
+
+    async def disconnect(self, code):
+        return await super().disconnect(code)
